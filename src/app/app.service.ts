3acf1cf3db50fbaa6236299c6399c476660706c2
Devi Lal <devi.lal@synapse.com>
2018-11-27 19:27:30 +0530

    addeed end point in environment

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import {Attribute, ValuesList} from './interfaces';
-
+import { environment} from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AppService {
     constructor(private http: HttpClient) { }
-    public endPoint = 'http://demo3385136.mockable.io/';
     public attributesUrl = 'attributes';
     public valuesListUrl = 'values-list';
     getAttributes(): Observable<Attribute[]> {
-        return this.http.get<Attribute[]>(this.endPoint + this.attributesUrl);
+        return this.http.get<Attribute[]>(environment.endPoint + this.attributesUrl);
     }
     getValuesList(): Observable<ValuesList[]> {
-        return this.http.get<ValuesList[]>(this.endPoint + this.valuesListUrl);
+        return this.http.get<ValuesList[]>(environment.endPoint + this.valuesListUrl);
     }
 }
